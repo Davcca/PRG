0b4770c82f39eeeccd782536d72d7b93aacb5367
Davcca <prochazka302@gmail.com>
2023-10-10 11:36:28 +0200

    dalsi hodina

---
+++ RecursionPlayground/RecursionPlayground/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RecursionPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
+            int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
+            int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
+            Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            Console.ReadKey();
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        static int Fibonacci(int n, int first = 0, int second = 1)
+        {
+            if (n == 0)
+            {
+                return first;
+            }
+            return Fibonacci(n - 1, second, first + second);
+        }
+    }
+}
