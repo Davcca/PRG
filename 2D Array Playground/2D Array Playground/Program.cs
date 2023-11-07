14cffbd5a4c41a8ae847b189dbc5fd4007189b5d
unknown <prochazka302@gmail.com>
2023-11-07 11:23:12 +0100

    dalsi hour

--- 2D Array Playground/2D Array Playground/Program.cs
+++ 2D Array Playground/2D Array Playground/Program.cs
@@ -23,7 +23,7 @@ namespace _2D_Array_Playground
                 for (int j = 0; j < my2DArray.GetLength(1); j++)
                 {
                     my2DArray[i, j] = i * 5 + j + 1;
-                    Console.WriteLine(i * 5 + j + 1);
+                    Console.WriteLine(my2DArray[i, j]);
                 }
                 Console.WriteLine("\n");
             }
@@ -53,7 +53,7 @@ namespace _2D_Array_Playground
 
             //Vedlejší diagonála
             Console.WriteLine("vedlejší");
-            for (int i = 4; i > -1 ; i--)
+            for (int i = 4; i > -1; i--)
             {
                 Console.WriteLine(my2DArray[i, 4 - i]);
             }
@@ -61,23 +61,94 @@ namespace _2D_Array_Playground
 
             //TODO 4: Prohoď prvek na souřadnicích [xFirst, yFirst] s prvkem na souřadnicích [xSecond, ySecond] a vypiš celé pole do konzole po prohození.
             //Nápověda: Budeš potřebovat proměnnou navíc, do které si uložíš první z prvků před tím, než ho přepíšeš druhým, abys hodnotou prvního prvku potom mohl přepsat druhý
-            int xFirst, yFirst, xSecond, ySecond;
-
+            int xFirst = 0;
+            int yFirst = 4;
+            int xSecond = 1;
+            int ySecond = 1;
+            int first = my2DArray[xFirst, yFirst];
+            int second = my2DArray[xSecond, ySecond];
+            int temp = first;
+            my2DArray[xFirst, yFirst] = second;
+            my2DArray[xSecond, ySecond] = temp;
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < my2DArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(my2DArray[i, j]);
+                }
+            }
+            Console.WriteLine("\n");
             //TODO 5: Prohoď n-tý řádek v poli s m-tým řádkem (n je dáno proměnnou nRowSwap, m mRowSwap) a vypiš celé pole do konzole po prohození.
             int nRowSwap = 0;
             int mRowSwap = 1;
+            int[] tempArray = new int[5];
+
+            for (int j = 0; j < my2DArray.GetLength(1); j++)
+            {
+                tempArray[j] = my2DArray[nRowSwap, j];
+            }
+
+            for (int j = 0; j < my2DArray.GetLength(1); j++)
+            {
+                my2DArray[nRowSwap, j] = my2DArray[mRowSwap, j];
+            }
+            for (int j = 0; j < my2DArray.GetLength(1); j++)
+            {
+                my2DArray[mRowSwap, j] = tempArray[j];
+            }
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < my2DArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(my2DArray[i, j]);
+                }
+            }
+            Console.WriteLine("\n");
 
             //TODO 6: Prohoď n-tý sloupec v poli s m-tým sloupcem (n je dáno proměnnou nColSwap, m mColSwap) a vypiš celé pole do konzole po prohození.
             int nColSwap = 0;
-            int mColSwap = 1;
+            int mColSwap = 3;
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                tempArray[i] = my2DArray[i, nColSwap];
+            }
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                my2DArray[i, nColSwap] = my2DArray[i, mColSwap];
+            }
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                my2DArray[i, nColSwap] = tempArray[i];
+            }
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < my2DArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(my2DArray[i, j]);
+                }
+            }
+
 
             //TODO 7: Otoč pořadí prvků na hlavní diagonále (z levého horního rohu do pravého dolního rohu) a vypiš celé pole do konzole po otočení.
+            for (int i = 0; i <= my2DArray.GetLength(0) / 2; i++)
+            {
+                int temp1 = my2DArray[i, i];
+                int reversedIndex = my2DArray.GetLength(0) - i - 1;
+                my2DArray[i, i] = my2DArray[reversedIndex, reversedIndex];
+                my2DArray[reversedIndex, reversedIndex] = temp1;
 
+            }
 
             //TODO 8: Otoč pořadí prvků na vedlejší diagonále (z pravého horního rohu do levého dolního rohu) a vypiš celé pole do konzole po otočení.
 
-
-            Console.ReadKey();
+            for (int i = 4; i <= my2DArray.GetLength(0) / 2; i--)
+            {
+                int temp1 = my2DArray[i, my2DArray.GetLength(0 - i - 1)];
+                int reversedIndex = my2DArray.GetLength(0) - i - 1;
+                my2DArray[i, i] = my2DArray[reversedIndex, reversedIndex];
+                my2DArray[reversedIndex, reversedIndex] = temp1;
+                Console.ReadKey();
+            }
         }
     }
-}
+
